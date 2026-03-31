c597825b47b9a1bf6a2401df9490eab32ddaceb2
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-03-31 18:03:19 +0600

    remove useless code

--- routes/niaz.php
+++ routes/niaz.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\CustomerController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\Admin\PromoCodeController;
-use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 
