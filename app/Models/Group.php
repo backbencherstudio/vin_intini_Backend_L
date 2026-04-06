b01523497adb28a343f5c4f1dd0136b2f92b07c5
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-04-06 11:28:29 +0600

    make relationship in group model

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'creator_id', 'is_private', 'logo', 'cover_photo'];
+
+    // Group members
+    public function members()
+    {
+        return $this->belongsToMany(User::class)->withPivot('role')->withTimestamps();
+    }
+
+    // Group creator
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
 }
