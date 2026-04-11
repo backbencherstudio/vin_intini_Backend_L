92030b80f13a761cdfb07dfa45deb750c7878ba9
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-04-11 10:10:41 +0600

    Added title in user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
+            $table->string('title')->nullable();
             $table->string('profile_image')->nullable();
             $table->string('email')->unique();
             $table->string('mobile')->nullable();
