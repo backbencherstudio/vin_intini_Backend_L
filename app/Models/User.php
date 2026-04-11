14f8d650c8b67d2ad098f248b00bf189f34077c7
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-04-11 11:09:22 +0600

    make relationship with userProfile table

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasMany(SocialAccount::class);
     }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
 }
