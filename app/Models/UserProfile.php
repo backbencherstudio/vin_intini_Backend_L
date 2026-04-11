441c189fd11e071d6ec91923900c50a28e9d8ff2
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-04-11 11:08:11 +0600

    Make userProfile model and right code

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserProfile extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'country',
+        'postal_code',
+        'profession',
+        'highest_degree',
+        'study_category',
+        'study_subcategory',
+        'institution',
+        'graduation_year',
+        'interests',
+        'about'
+    ];
+
+    protected $casts = [
+        'profession' => 'array',
+        'interests' => 'array',
+    ];
 }
