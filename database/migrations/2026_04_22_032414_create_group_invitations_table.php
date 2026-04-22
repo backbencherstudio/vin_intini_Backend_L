71ab9fb6d6ef5f34814e939dcdbd9456413c9243
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-04-22 10:54:21 +0600

    create group invitation table and model

---
+++ database/migrations/2026_04_22_032414_create_group_invitations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_invitations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('inviter_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('invited_user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+
+            $table->unique(['group_id', 'invited_user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_invitations');
+    }
+};
