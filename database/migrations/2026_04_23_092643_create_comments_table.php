53ac3be69b4de9d5a5745f1839d123bae976ad71
Shahadat-Hossain-Shanto <shahadathossain1563@gmail.com>
2026-04-23 17:34:10 +0600

    comment

--- database/migrations/2026_04_23_092643_create_comments_table.php
+++ database/migrations/2026_04_23_092643_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
 
-            $table->text('content');
+            $table->text('comment');
 
             $table->foreignId('parent_id')
                 ->nullable()
