dee7b0e40ed4f308250e8dbad9a4a81eb5befc9d
Shahadat-Hossain-Shanto <shahadathossain1563@gmail.com>
2026-05-02 16:35:14 +0600

    group list

--- routes/niaz.php
+++ routes/niaz.php
@@ -25,6 +25,8 @@ Route::middleware(['auth:api', 'role:user'])->group(function () {
     Route::get('/my-created-groups', [GroupController::class, 'myCreatedGroups']);
     // my joined group routes
     Route::get('/my-joined-groups', [GroupController::class, 'myJoinedGroups']);
+
+    Route::get('/group-list', [GroupController::class, 'myJoinedGroups']);
     // group member join and leave routes
     Route::post('/group/join', [GroupController::class, 'joinGroup']);
     Route::post('/group/leave', [GroupController::class, 'leaveGroup']);
