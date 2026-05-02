dad6954a12be3902c79be51d493c410c11fdd051
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-05-02 12:10:42 +0600

    Update academia_facilities table schema and model

--- app/Models/AcademiaFacility.php
+++ app/Models/AcademiaFacility.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class AcademiaFacility extends Model
 {
     protected $table = 'academia_facilities';
-    protected $fillable = ['state_id', 'name', 'location', 'type', 'university_id'];
+    protected $fillable = ['state_id', 'name', 'location', 'type', 'university_id', 'latitude', 'longitude'];
 
     public function university() {
         return $this->belongsTo(AcademiaUniversity::class);
