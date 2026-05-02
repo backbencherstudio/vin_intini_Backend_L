1fc7933fd819af68c75b824dd25fac6e135230ee
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-05-02 17:06:46 +0600

    Update

---
+++ database/migrations/2026_05_02_110554_update_user_profiles_table_for_companies_and_institutes.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('user_profiles')->update(['current_position_id' => null]);
+
+        Schema::table('user_profiles', function (Blueprint $table) {
+            try {
+                $table->dropForeign(['current_position_id']);
+            } catch (\Exception $e) {
+            }
+
+            $table->foreign('current_position_id')
+                ->references('id')
+                ->on('companies')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->dropForeign(['current_position_id']);
+
+            $table->foreign('current_position_id')
+                ->references('id')
+                ->on('experiences')
+                ->onDelete('set null');
+        });
+    }
+};
