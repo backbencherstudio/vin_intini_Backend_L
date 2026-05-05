40de316680afadcf13e071fb9356a19ab98bfa58
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-05-05 12:18:32 +0600

    Added new cors

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'http://localhost:3000'
+    ],
 
     'allowed_origins_patterns' => [],
 
