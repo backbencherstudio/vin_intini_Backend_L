0b6057a797209853da3d049aaf337f20d3a60cb9
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-05-16 16:52:18 +0600

    Update PostLikedNotification.php and added user profile url

--- app/Notifications/PostLikedNotification.php
+++ app/Notifications/PostLikedNotification.php
@@ -34,6 +34,7 @@ class PostLikedNotification extends Notification implements ShouldQueue
         return [
             'sender_id'    => $this->sender->id,
             'sender_name'  => $this->sender->first_name . ' ' . $this->sender->last_name,
+            'sender_profile_image_url'  => $this->sender->profile_image_url,
             'post_id'      => $this->post->id,
             'type'         => class_basename(self::class),
             'message'      => 'liked your post',
