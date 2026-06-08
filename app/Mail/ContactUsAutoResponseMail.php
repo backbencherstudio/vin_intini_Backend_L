a5b5992e887458d898b3a6f3f3723da9c23d3ae6
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-06-08 10:29:56 +0600

    create contact us auto response mail

---
+++ app/Mail/ContactUsAutoResponseMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactUsAutoResponseMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $contactData;
+
+    public function __construct($contactData)
+    {
+        $this->contactData = $contactData;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Thank you for contacting MindUnite',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact_us_auto_response',
+        );
+    }
+}
