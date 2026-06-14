19585f968ee84e552c617ff7d225809ab4f0c0f1
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-06-14 18:20:48 +0600

    create partnar model and schema file

---
+++ app/Models/IndustryPartner.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IndustryPartner extends Model
+{
+    protected $fillable = ['network_type', 'industry_type', 'partner_name', 'partner_tag', 'partner_desc', 'partner_link'];
+}
