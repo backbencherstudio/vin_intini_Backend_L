eb0e007756e07b3b445051f6c0bc59ec9894cbcd
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-06-18 16:07:52 +0600

    Update image path in model

--- app/Models/IndustryItem.php
+++ app/Models/IndustryItem.php
@@ -3,16 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class IndustryItem extends Model
 {
     protected $fillable = [
-        'category_id', 'title', 'tag', 'sub_title', 'indication',
-        'moa', 'pub_date', 'extra_tag', 'image', 'description', 'link'
+        'category_id',
+        'title',
+        'tag',
+        'sub_title',
+        'indication',
+        'moa',
+        'pub_date',
+        'extra_tag',
+        'image',
+        'description',
+        'link'
     ];
 
-    public function IndustryCategory() {
+    public function IndustryCategory()
+    {
         return $this->belongsTo(IndustryCategory::class, 'category_id');
+    }
+
+    protected $appends = ['image_url'];
+
+    protected function imageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->image ? asset('storage/' . $this->image) : null,
+        );
     }
 
     protected $hidden = ['created_at', 'updated_at'];
