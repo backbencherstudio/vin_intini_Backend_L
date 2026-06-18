3cb34e744164def5a72ba96ea405898643ae8df3
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-06-18 17:08:48 +0600

    Update route

--- routes/web.php
+++ routes/web.php
@@ -117,16 +117,16 @@ Route::prefix('api/neuroscience-network/industry')->group(function () {
 });
 
 // User Management Routes
-Route::get('/niaz/users', [AdminAuthController::class, 'allUsers'])->name('users.index');
-Route::post('/niaz/users/update', [AdminAuthController::class, 'userUpdate'])->name('users.update');
-Route::delete('/niaz/users/{id}', [AdminAuthController::class, 'userDestroy'])->name('users.destroy');
+Route::get('/ni-az/users', [AdminAuthController::class, 'allUsers'])->name('users.index');
+Route::post('/ni-az/users/update', [AdminAuthController::class, 'userUpdate'])->name('users.update');
+Route::delete('/ni-az/users/{id}', [AdminAuthController::class, 'userDestroy'])->name('users.destroy');
 
-Route::get('/niaz/groups', [AdminAuthController::class, 'groupIndex'])->name('groups.index');
-Route::post('/niaz/groups/update', [AdminAuthController::class, 'groupUpdate'])->name('groups.update');
-Route::delete('/niaz/groups/{id}', [AdminAuthController::class, 'groupDestroy'])->name('groups.destroy');
+Route::get('/ni-az/groups', [AdminAuthController::class, 'groupIndex'])->name('groups.index');
+Route::post('/ni-az/groups/update', [AdminAuthController::class, 'groupUpdate'])->name('groups.update');
+Route::delete('/ni-az/groups/{id}', [AdminAuthController::class, 'groupDestroy'])->name('groups.destroy');
 
-Route::get('/niaz/posts', [AdminAuthController::class, 'postIndex'])->name('posts.index');
-Route::post('/niaz/posts/update', [AdminAuthController::class, 'postUpdate'])->name('posts.update');
-Route::delete('/niaz/posts/{id}', [AdminAuthController::class, 'postDestroy'])->name('posts.destroy');
+Route::get('/ni-az/posts', [AdminAuthController::class, 'postIndex'])->name('posts.index');
+Route::post('/ni-az/posts/update', [AdminAuthController::class, 'postUpdate'])->name('posts.update');
+Route::delete('/ni-az/posts/{id}', [AdminAuthController::class, 'postDestroy'])->name('posts.destroy');
 
 require __DIR__ . '/auth.php';
