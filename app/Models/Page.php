e319a93c3910458161c5875911ced9111fb3cd68
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-07-16 16:03:49 +0600

    Add Page model and migration for pages table

---
+++ app/Models/Page.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Page extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'vision',
+        'mission',
+        'strategy',
+        'what_we_do_image',
+        'team_members',
+        'features_videos',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'team_members' => 'array',
+        'features_videos' => 'array',
+        'images' => 'array',
+    ];
+}
---
+++ database/migrations/2026_07_16_040131_create_pages_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+
+            $table->longText('content')->nullable();
+
+            $table->longText('vision')->nullable();
+            $table->longText('mission')->nullable();
+            $table->longText('strategy')->nullable();
+
+            $table->string('what_we_do_image')->nullable();
+
+            $table->json('team_members')->nullable();
+
+            $table->json('features_videos')->nullable();
+
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
