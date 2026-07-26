74714f2e0ebc87a96bdb1197ff83a1207d251988
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-07-26 13:17:22 +0600

    Refactor About Us page structure in PageSeeder to enhance founder information and update team member bios

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -14,84 +14,70 @@ class PageSeeder extends Seeder
             [
                 'title' => 'About Us',
                 'slug'  => 'about-us',
-                'founder_photo' => null, 
-                'founder_bio' => 'Hello, my name is Vanni Intini and I am the founder and CEO of Mind Unite. I created Mind Unite to bridge the gap between brain health professionals and industry partners, fostering a community of collaboration and innovation.',
                 'content' => 'At Mind Unite, we attract Brain Health professionals and partner with Biotech, Psychotropic, and Publication Companies to create an environment for collaboration, resource optimization, and opportunity sharing in one location.',
                 'vision'  => 'To create and grow a brain health professionals networking platform into a global collaboration and resource sharing enterprise.',
                 'mission' => 'To connect current and future brain health professionals to information, resources, and opportunities.',
                 'strategy' => 'Together we will build an online collaboration platform that will be used exclusively by current and future brain health professionals, and those industry partners who empower them, into the global networking platform in which users can thrive.',
+
+                'founder_info' => [
+                    'name'        => 'Vanni Intini',
+                    'designation' => 'Founder & CEO of Mind Unite',
+                    'bio'         => 'Hello, my name is Vanni Intini and I am the founder and CEO of Mind Unite. I created Mind Unite to bridge the gap between brain health professionals and industry partners.',
+                    'photo'       => null,
+                    'signature'   => 'Vanni Intini'
+                ],
+
                 'what_we_do_image' => null,
+
                 'team_members' => [
                     [
                         'name' => 'Vanni Intini',
                         'title' => 'Founder & CEO',
-                        'bio' => 'Vanni is an undergraduate student at the University of North Carolina Chapel Hill Double Majoring in Psychology and Neuroscience.',
+                        'bio' => 'Vanni is an undergraduate student at UNC Chapel Hill.',
                         'photo' => null
                     ],
                     [
                         'name' => 'Macy Johnson',
                         'title' => 'Co-Founder',
-                        'bio' => 'An undergraduate student passionate about brain health and networking platforms.',
+                        'bio' => 'An undergraduate student passionate about brain health.',
                         'photo' => null
                     ]
                 ],
+
                 'features_videos' => [
                     [
-                        'title' => 'Tutorial 1',
-                        'source' => 'url',
-                        'url' => 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-                        'path' => null
-                    ],
+                        'title'     => 'Introduction Video',
+                        'source'    => 'url',
+                        'url'       => 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+                        'path'      => null,
+                        'thumbnail' => null
+                    ]
+                ],
+
+                'faqs' => [
                     [
-                        'title' => 'Tutorial 2',
-                        'source' => 'url',
-                        'url' => 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-                        'path' => null
+                        'question' => 'What is Mind Unite?',
+                        'answer'   => 'Mind Unite is a networking platform for brain health professionals.'
                     ]
                 ],
             ],
             [
                 'title' => 'Privacy Policy',
                 'slug'  => 'privacy-policy',
-                'content' => '<h3>Privacy Policy</h3><p>This is the default privacy policy content. We respect your data and privacy.</p>',
-                'vision' => null,
-                'mission' => null,
-                'strategy' => null,
+                'content' => '<h3>Privacy Policy</h3><p>We respect your data and privacy.</p>',
+                'founder_info' => null,
                 'team_members' => null,
                 'features_videos' => null,
+                'faqs' => null,
             ],
             [
                 'title' => 'Terms & Conditions',
                 'slug'  => 'terms-and-conditions',
-                'content' => '<h3>Terms and Conditions</h3><p>By using our website, you agree to these terms. Please read carefully.</p>',
-                'vision' => null,
-                'mission' => null,
-                'strategy' => null,
+                'content' => '<h3>Terms and Conditions</h3><p>By using our website, you agree to these terms.</p>',
+                'founder_info' => null,
                 'team_members' => null,
                 'features_videos' => null,
-            ],
-            [
-                'title' => 'Guidelines',
-                'slug'  => 'how-to-use',
-                'content' => 'Follow these instructions to use the platform effectively.',
-                'vision' => null,
-                'mission' => null,
-                'strategy' => null,
-                'team_members' => null,
-                'features_videos' => [
-                    [
-                        'title' => 'How to Login',
-                        'source' => 'url',
-                        'url' => 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-                        'path' => null
-                    ],
-                    [
-                        'title' => 'How to Post',
-                        'source' => 'url',
-                        'url' => 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-                        'path' => null
-                    ]
-                ],
+                'faqs' => null,
             ],
         ];
 
