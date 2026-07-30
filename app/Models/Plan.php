59d81d8fb9fc3889c9c383c78f3b39445ddcf95d
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-07-30 17:14:50 +0600

    Create Plan model with casts

---
+++ app/Models/Plan.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $fillable = [
+        'name',
+        'short_description',
+        'billing_rate',
+        'billing_cycle',
+        'discount_percent',
+        'discount_duration',
+        'badge_color',
+        'status',
+        'features',
+        'stripe_product_id',
+        'stripe_price_id',
+    ];
+
+    protected $attributes = [
+        'status' => 'active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'features' => 'array',
+            'billing_rate' => 'decimal:2',
+            'discount_percent' => 'decimal:2',
+            'discount_duration' => 'date',
+        ];
+    }
+}
