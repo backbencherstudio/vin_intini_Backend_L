33092c14e51b3d8a04acbc22f1ad15b0d47a6f14
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-08-11 08:48:29 +0600

    Remove commented-out code for two-factor authentication reset in verifyLogin method

--- app/Http/Controllers/Api/TwoFactorController.php
+++ app/Http/Controllers/Api/TwoFactorController.php
@@ -280,9 +280,9 @@ class TwoFactorController extends Controller
         if ($user && (string)$user->otp === (string)$request->otp && now()->lt($user->otp_expires_at)) {
 
             $user->update([
-                'two_factor_confirmed_at' => null,
-                'two_factor_secret' => null,
-                'two_factor_recovery_codes' => null,
+                // 'two_factor_confirmed_at' => null,
+                // 'two_factor_secret' => null,
+                // 'two_factor_recovery_codes' => null,
                 'otp' => null,
                 'otp_expires_at' => null
             ]);
