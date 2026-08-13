fd850ce8bc57859c6dfb6e5dc8b3226d522d1ad0
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-08-13 11:46:36 +0600

    Register integration settings API routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\Admin\PlanController;
-use App\Http\Controllers\Admin\SubscriptionManagementController;
-use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Admin\Api\IntegrationSettingController;
+use App\Http\Controllers\Admin\Api\PlanController;
+use App\Http\Controllers\Admin\Api\SubscriptionManagementController;
+use App\Http\Controllers\Admin\Api\TransactionController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
-    //change password route
+    // change password route
     Route::post('/update-password', [UserController::class, 'updatePass']);
     // Route::put('/profile-update', [UserController::class, 'profileUpdate']);
+
+    // integration settings routes
+    Route::get('/integrations', [IntegrationSettingController::class, 'index']);
+    Route::post('/integrations', [IntegrationSettingController::class, 'update']);
 
     // plan routes
     Route::get('/plans', [PlanController::class, 'index']);
