b161d76d2bd0528ef82685ec06f98d74e0f34a29
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-08-19 18:04:41 +0600

    Add username field to User model and migration for enhanced user identification

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements JWTSubject
     protected $fillable = [
         'first_name',
         'last_name',
+        'username',
         'title',
         'email',
         'mobile',
