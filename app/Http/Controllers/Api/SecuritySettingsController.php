fb1a4439edda761ef158f816b4d7d5a680344456
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-08-24 15:26:17 +0600

    refactor: Remove unused User model import in SecuritySettingsController

--- app/Http/Controllers/Api/SecuritySettingsController.php
+++ app/Http/Controllers/Api/SecuritySettingsController.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Validation\Rules\Password;
 use App\Models\DeletedAccountLog;
-use App\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class SecuritySettingsController extends Controller
