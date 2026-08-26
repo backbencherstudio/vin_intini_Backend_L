63e43a04fdc916d579636692e2a89f5bd534b11a
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-08-26 14:30:52 +0600

    add Industry model

--- app/Models/Industry.php
+++ app/Models/Industry.php
@@ -2,9 +2,47 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Industry extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'industry_category_id',
+        'website',
+        'address',
+        'company_size',
+        'logo',
+        'cover_image',
+        'tagline',
+        'authorization_confirmed',
+        'authorization_confirmed_at',
+        'created_by',
+    ];
+
+    protected $casts = [
+        'authorization_confirmed' => 'boolean',
+        'authorization_confirmed_at' => 'datetime',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(
+            IndustryCategory::class,
+            'industry_category_id'
+        );
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(
+            User::class,
+            'created_by'
+        );
+    }
 }
