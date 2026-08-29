b21e0210a8959799fc41c9634c3d794ea8ed6d5a
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-08-29 11:59:55 +0600

    add RecruiterPost model fill with relation

--- app/Models/RecruiterPost.php
+++ app/Models/RecruiterPost.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RecruiterPost extends Model
 {
-    //
+    protected $fillable = [
+        'industry_id',
+        'created_by',
+        'content',
+    ];
+
+    public function industry(): BelongsTo
+    {
+        return $this->belongsTo(Industry::class);
+    }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function media(): HasMany
+    {
+        return $this->hasMany(RecruiterPostMedia::class)
+            ->orderBy('sort_order');
+    }
 }
