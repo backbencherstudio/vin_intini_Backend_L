1b0fb2555445072f879906ebc162931ad1a23038
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-08-29 12:00:35 +0600

    add RecruiterPostMedia model fill with relation

--- app/Models/RecruiterPostMedia.php
+++ app/Models/RecruiterPostMedia.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RecruiterPostMedia extends Model
 {
-    //
+    protected $fillable = [
+        'recruiter_post_id',
+        'type',
+        'path',
+        'sort_order',
+    ];
+
+    public function recruiterPost(): BelongsTo
+    {
+        return $this->belongsTo(
+            RecruiterPost::class,
+            'recruiter_post_id'
+        );
+    }
 }
