070d7be6a82b77ce9777dd017d18d38caf6a34d9
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-09-02 17:18:29 +0600

    Add 'has_password' field to user model and update password handling logic

--- app/Http/Controllers/Api/SocialController.php
+++ app/Http/Controllers/Api/SocialController.php
@@ -78,6 +78,7 @@ class SocialController extends Controller
                         'is_verified' => true,
                         'profile_image' => $avatarUrl !== '' ? $avatarUrl : null,
                         'password' => Str::random(32),
+                        'has_password' => false,
                         'terms_accepted_at' => now(),
                     ]);
                 }
