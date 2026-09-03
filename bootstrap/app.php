8c85b4c179badcb7c09b8e7140913e88b1dbe5b5
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-09-03 17:53:38 +0600

    Add trustProxies middleware configuration to app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,6 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
 
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->append(HandleCors::class);
 
         $middleware->alias([
