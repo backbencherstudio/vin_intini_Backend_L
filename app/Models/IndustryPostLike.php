ee1955bd936b0a01b68e25638960b3eeb6dbd690
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-09-07 12:07:36 +0600

    RecruiterPostLike rename and modify

--- app/Models/IndustryPostLike.php
+++ app/Models/IndustryPostLike.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class RecruiterPostLike extends Model
+class IndustryPostLike extends Model
 {
     protected $fillable = [
         'post_id',
@@ -14,7 +14,7 @@ class RecruiterPostLike extends Model
     public function post()
     {
         return $this->belongsTo(
-            RecruiterPost::class,
+            IndustryPost::class,
             'post_id'
         );
     }
