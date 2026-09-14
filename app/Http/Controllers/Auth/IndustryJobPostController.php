50a103fe9433791e2e4fb3564c0c54ad2cbeb557
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-09-14 12:24:43 +0600

    add initial IndustryJobPostController

--- app/Http/Controllers/Auth/IndustryJobPostController.php
+++ app/Http/Controllers/Auth/IndustryJobPostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\JobPost;
+use App\Enums\JobPostStatus;
 use Illuminate\Http\Request;
 
 class IndustryJobPostController extends Controller
